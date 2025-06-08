cff460d8c0ded5276d132c83c7750a14763676e4
rishit puri <60522475+rishitpuri@users.noreply.github.com>
2025-06-08 03:08:50 -0400

    Add files via upload

---
+++ Create_RAW_Data.sql
@@ -0,0 +1,17 @@
+# Rishit Puri 
+CREATE TABLE raw_data (
+    bid INT(3),
+    title VARCHAR(255),
+    author VARCHAR(255),
+    category VARCHAR(255),
+    book_status VARCHAR(255),
+    category_id INT(2),
+    shelf_id INT(2),
+    author_id INT(2),
+    transaction_id INT(2),
+    Student_Name VARCHAR(255),
+    Student_ID INT(2),
+    major VARCHAR(255),
+    age INT,
+    Issued_Date DATE
+);
